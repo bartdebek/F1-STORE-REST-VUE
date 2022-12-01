99edaad1264627dbaf1143e0665b03a465af57ff
debus3 <debus3@gmail.com>
2022-12-01 20:04:37 +0100

    add heroku to allowed hosts

--- f1store/settings.py
+++ f1store/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = ['f1companion.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['f1-store.herokuapp.com', 'localhost', '127.0.0.1']
 
 STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
 
