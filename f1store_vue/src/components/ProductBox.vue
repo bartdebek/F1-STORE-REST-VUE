f0a5e1283cc64a499727ae9f0a527656200d983f
Bartosz Dębek <debus3@gmail.com>
2022-12-05 22:04:22 +0100

    cosmetic change to team panels, add links to product and team images

--- f1store_vue/src/components/ProductBox.vue
+++ f1store_vue/src/components/ProductBox.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="column is-3">
         <div class="box">
+          <router-link v-bind:to="product.get_absolute_url">
             <figure class="image mb-4">
                 <img v-bind:src="product.get_thumbnail">
             </figure>
-
+          </router-link>
             <h3 class="is-size-4">{{ product.name }}</h3>
             <p class="is-size-6 has-text-grey">${{ product.price }}</p>
 
--- f1store_vue/src/components/TeamBox.vue
+++ f1store_vue/src/components/TeamBox.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="column is-3">
         <div class="box">
+          <router-link v-bind:to="team.get_absolute_url">
             <figure class="image mb-4">
                 <img v-bind:src="team.get_thumbnail">
             </figure>
-
-            <h3 class="is-size-4 center">{{ team.name }}</h3>
+          </router-link>
             <router-link v-bind:to="team.get_absolute_url" class="button is-dark mt-4">View products</router-link>
         </div>
     </div>
